07c1131dd5f7dbddfa9be7512a6c0b444c0492c6
SinxLinonZ <urara.kbys@gmail.com>
2025-03-09 02:44:21 +0900

    Change weather layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 import Clock from './components/Clock/Clock';
 import Calendar from './components/Calendar/Calendar';
+import Weather from './components/Weather/Weather';
 
 function App() {
-  const [reloadKey, setReloadKey] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setReloadKey(prevKey => prevKey + 1);
-    }, 30 * 60 * 1000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <>
@@ -20,14 +11,7 @@ function App() {
         <Clock />
         <Calendar />
 
-        <div id="weather_current" key={reloadKey}>
-          <div id="ww_76ea7ae5d5835" v='1.3' loc='id' a='{"t":"horizontal","lang":"zh","sl_lpl":1,"ids":["wl4480"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'><a href="https://weatherwidget.org/" id="ww_76ea7ae5d5835_u" target="_blank">Weather widget html</a></div><script async src="https://app3.weatherwidget.org/js/?id=ww_76ea7ae5d5835"></script>
-        </div>
-
-        <div id="weather_forecast" key={reloadKey}>
-          <div id="ww_e1234bbda49f9" v='1.3' loc='id' a='{"t":"responsive","lang":"zh","sl_lpl":1,"ids":["wl4480"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722","el_cwi":3,"el_nme":3,"el_ctm":3}'><a href="https://weatherwidget.org/" id="ww_e1234bbda49f9_u" target="_blank">Weather widget html</a></div><script async src="https://app3.weatherwidget.org/js/?id=ww_e1234bbda49f9"></script>
-        </div>
-
+        <Weather />
       </div>
     </>
   );
---
+++ src/components/Weather/Weather.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Weather.css';
+
+function Weather() {
+
+  const srcDoc_weather =
+    `<div id="ww_76ea7ae5d5835" v='1.3' loc='id' a='{"t":"horizontal","lang":"zh","sl_lpl":1,"ids":["wl4480"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'><a href="https://weatherwidget.org/" id="ww_76ea7ae5d5835_u" target="_blank">Weather widget html</a></div><script async src="https://app3.weatherwidget.org/js/?id=ww_76ea7ae5d5835"></script>
+  <div id="ww_e1234bbda49f9" v='1.3' loc='id' a='{"t":"responsive","lang":"zh","sl_lpl":1,"ids":["wl4480"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722","el_cwi":3,"el_nme":3,"el_ctm":3}'><a href="https://weatherwidget.org/" id="ww_e1234bbda49f9_u" target="_blank">Weather widget html</a></div><script async src="https://app3.weatherwidget.org/js/?id=ww_e1234bbda49f9"></script>
+  <style>
+    body {
+      font-weight: bold;
+      width: fit-content;
+      background-color: #242424;
+      transform-origin: top left;
+      transform: scale(1.5);
+    }
+    #ww_e1234bbda49f9 {
+      width: 440px;
+    }
+  </style>
+  <script>
+    setTimeout(() => {
+      document.location.reload();
+    }, 30 * 10 * 1000);
+  </script>
+  `;
+
+  return (
+    <>
+      <iframe
+        id="weather"
+        srcdoc={srcDoc_weather}
+        style={{ border: 'none', width: '100%', height: '100%', margin: '0', padding: '0' }}
+      ></iframe>
+    </>
+  );
+}
+
+export default Weather;
